2d09d134b647702fbd1ecaa01b1f1cab51690056
IngridCBarbosa <ingridcarolinelima@hotmail.com>
2021-05-30 15:02:33 -0300

    Added search filter by city, query and category in restaurant

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -3,14 +3,32 @@ class RestaurantsController < ApplicationController
   
   def index
     @restaurants = Restaurant.all
+    filter_by_query if params[:query]
+    filter_by_city if params[:city]
+    filter_by_category if params[:category]
   end
 
   def show
   end
 
+  # todos os métodos abaixo são privados
   private
 
   def set_restaurant
     @restaurant = Restaurant.find(params[:id])
+  end
+
+  def filter_by_query
+    @restaurants = @restaurants.ransack(name_or_description_cont: params[:query]).result
+  end
+
+  def filter_by_city
+    @restaurants = @restaurants.where(city: params[:city])
+  end
+
+  def filter_by_category
+    @restaurants = @restaurants.select do |restaurant|
+      restaurant.category.title == params[:category]
+    end
   end
 end
